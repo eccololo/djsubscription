32da45ecbddd60e83d869907eb75945721716640
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-07-14 01:18:27 +0200

    Add custom user model to account app.

--- subplatform/account/models.py
+++ subplatform/account/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+
+    username = None
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=135)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    is_writer = models.BooleanField(default=False, verbose_name="Are you a writer?")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        
+        return self.email
