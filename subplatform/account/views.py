93a7617485109a7d1a13a52c156b8f637805643a
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-07-15 21:05:40 +0200

    Add basic templates to account app.

--- subplatform/account/views.py
+++ subplatform/account/views.py
@@ -3,14 +3,26 @@ from django.shortcuts import render
 
 def home(request):
 
-    pass
+    context = {
+
+    }
+
+    return render(request, "account/index.html", context)
 
 
 def register(request):
 
-    pass
+    context = {
+
+    }
+
+    return render(request, "account/register.html", context)
 
 
 def my_login(request):
 
-    pass
+    context = {
+
+    }
+
+    return render(request, "account/my-login.html", context)
