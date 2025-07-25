04456c3c11ff2d18d94b68eb1b90d9746eaf2b09
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-07-25 14:02:39 +0200

    Add logout functionality to client and writer apps.

--- subplatform/account/views.py
+++ subplatform/account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 from .forms import CreateUserForm
 
@@ -70,3 +70,14 @@ def my_login(request):
     }
 
     return render(request, "account/my-login.html", context)
+
+
+def user_logout(request):
+
+    logout(request)
+
+    return redirect("my-login")
+
+
+
+
