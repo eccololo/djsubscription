fd64f7904306c3fd5c6c4d342140215a917d76bd
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-07-26 13:44:43 +0200

    Add Article model to writer app.

--- subplatform/writer/models.py
+++ subplatform/writer/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from account.models import CustomUser
+
+class Article(models.Model):
+
+    title = models.CharField(max_length=150)
+    content = models.TextField(max_length=10000)
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    is_premium = models.BooleanField(default=False, verbose_name="Is this article premium?")
+
+    user = models.ForeignKey(CustomUser, max_length=25, on_delete=models.CASCADE, null=True)
+
+
+
