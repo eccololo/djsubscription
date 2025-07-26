f979566a1458a6ac51ca9e59bf8d3d2acfe68ecf
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-07-26 13:53:08 +0200

    Add form, view and url for creating article functionality.

--- subplatform/writer/views.py
+++ subplatform/writer/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
+from .forms import ArticleForm
 
 
 @login_required(login_url="my-login")
@@ -10,4 +13,29 @@ def writer_dashboard(request):
     }
 
     return render(request, "writer/writer-dashboard.html")
+
+
+
+@login_required(login_url="my-login")
+def create_article(request):
+
+    form = ArticleForm()
+
+    if request.method == "POST":
+
+        form = ArticleForm(request.POST)
+
+        if form.is_valid():
+
+            article = form.save(commit=False)
+            article.user = request.user
+            article.save()
+
+            return HttpResponse("Article was created and saved!")
+        
+    context = {
+        "CreateArticleForm": form
+    }
+
+    return render(request, "writer/create-article.html", context)
 
