eb0e42aa4e9ffe64f70917326530e86465e664e0
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-08-02 16:54:19 +0200

    Add browse subscription by client feature in client app.

--- subplatform/client/views.py
+++ subplatform/client/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+
+from writer.models import Article
+from .models import Subscription
 
 
 @login_required(login_url="my-login")
@@ -10,3 +13,32 @@ def client_dashboard(request):
     }
 
     return render(request, "client/client-dashboard.html")
+
+
+@login_required(login_url="my-login")
+def browse_articles(request):
+
+    try:
+
+        subDetail = Subscription.objects.get(user=request.user, is_active=True)
+    
+    except:
+
+        return redirect("client-dashboard")
+    
+    current_subscription_plan = subDetail.subscription_plan
+
+    if current_subscription_plan == "Standard":
+
+        articles = Article.objects.all().filter(is_premium=False)
+
+    elif current_subscription_plan == "Premium":
+
+        articles = Article.objects.all()
+
+    context = {
+        "AllClientsArticles": articles
+    }
+
+    return render(request, "client/browse-articles.html", context)
+
